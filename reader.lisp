4664d25d41043e2d8de1eeaeef81757c22b39f39
dan lentz <danlentz@gmail.com>
2012-01-20 11:47:50 -0500

    'standard-syntax' readtable fully implemented supporting macro-char {set}, {|map|}, [seq], dispatch macro char transactional #{set}, #{|map|}, #[seqs], and $ safe-value reader for agnostic operations on eitther tx or immutable without additional overhead or specialization

---
+++ reader.lisp
@@ -0,0 +1,62 @@
+;;;;; -*- mode: common-lisp;   common-lisp-style: modern;    coding: utf-8; -*-
+;;;;;
+
+(in-package :dclx)
+
+
+(defun delimiter-for (char)
+  (ecase char
+    (#\{ #\})
+    (#\[ #\])
+    (#\< #\>)
+    (#\( #\))))
+
+
+(defreadtable dclx:standard-syntax
+  (:merge :standard)
+  (:syntax-from :standard #\) #\})
+  (:syntax-from :standard #\) #\])
+  (:macro-char dclx:*value-reader-macro-char*
+    #'(lambda (stream char)
+        (declare (ignore char))
+        (dstm:safe-value (read stream))))
+  (:macro-char dclx:*set-reader-macro-char*
+    #'(lambda (stream char)
+        (declare (ignore char))
+        (case (peek-char nil stream t nil t)
+          (#\| (progn
+                 (read-char stream t nil t)
+                 (let ((contents (read-delimited-list #\| stream t)))
+                   (if (eql (read-char stream) #\})
+                     (map:make contents)
+                     (error "Invalid map collection syntax.")))))
+          (t    (set:make
+                  (read-delimited-list (delimiter-for dclx:*set-reader-macro-char*)  stream))))))
+  (:macro-char dclx:*seq-reader-macro-char*
+    #'(lambda (stream char)
+        (declare (ignore char))
+        (seq:make
+          (read-delimited-list (delimiter-for dclx:*seq-reader-macro-char*) stream))))
+  (:dispatch-macro-char #\# dclx:*set-reader-macro-char*
+    #'(lambda (stream char arg)
+        (declare (ignore char arg))
+        (case (peek-char nil stream t nil t)
+          (#\| (progn
+                 (read-char stream t nil t)
+                 (let ((contents (read-delimited-list #\| stream t)))
+                   (if (eql (read-char stream) #\})
+                     (map:make* contents)
+                     (error "Invalid map collection syntax.")))))
+          (t    (set:make*
+                  (read-delimited-list (delimiter-for dclx:*set-reader-macro-char*)  stream))))))
+  (:dispatch-macro-char #\# dclx:*seq-reader-macro-char*
+    #'(lambda (stream char arg)
+        (declare (ignore char arg))
+        (seq:make*
+          (read-delimited-list (delimiter-for dclx:*seq-reader-macro-char*) stream)))))
+
+
+
+
+
+
