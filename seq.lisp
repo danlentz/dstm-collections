11f595da65c93b20599f2a61bc8a21eb9a6525f3
dan lentz <danlentz@gmail.com>
2012-02-03 22:54:50 -0500

    seq*, mutable-seq/dstm and mutable-seq/cstm.  All ops updated with stm/nonstm agnostic behaviors. re-implement dup, mapi. concat; imlement map-=indices, reindex, reindex*, reverse, elt, reduce, ...

--- seq.lisp
+++ seq.lisp
@@ -1,22 +1,20 @@
 ;;;;; -*- mode: common-lisp;   common-lisp-style: modern;    coding: utf-8; -*-
 ;;;;;
 
-
 (in-package :seq)
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; class seq*
+;; seq collection classes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defclass seq* (map:map*)
+(defclass mutable-seq/dstm (map::mutable-map/dstm)
   ())
 
-(defmethod print-object ((s seq*) stream)
-  (let ((val (dstm:atomic (dstm:read s))))
-    (ord:writing-readably 
-      (format stream "#[ ~{~s ~}]" (seq:list val)))))
+(defclass mutable-seq/cstm (map::mutable-map/cstm)
+  ())
 
+(defclass seq* (map:map* mutable-seq/cstm)
+  ())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; seq element
@@ -45,11 +43,13 @@
 ;; seq:type
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 (defun seq:typep (thing)
   "seq collection type-predicate"
   (or
     (null thing)
-    (cl:typep thing 'seq*) 
+    (cl:typep thing 'mutable-seq/dstm)
+    (cl:typep thing 'mutable-seq/cstm) 
     (and (tree:typep thing) (cl:typep (tree:rb-tree-v thing) 'seq-cell))))
 
 
@@ -57,195 +57,293 @@
   "collection of arbitrary, possibly non-unique, elements in some specific lexical order"
   `(satisfies seq:typep))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; set:type
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defun set:typep (thing)
-  "set collection type-predicate"
-  (or
-    (null thing)
-    (cl:typep thing 'set:set*) 
-    (tree:typep thing)))
-
-
-(deftype set:type ()
-  "collection of arbitrary, unique, elements in order determined by defined ordinal
-   comparison relations on element types and content values"
-  `(satisfies set:typep))
-
-
+  
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; seq api
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun empty ()
+
+(defun seq::first-node (seq)
+  (let ((seq (value seq)))
+    (cond ((null seq) nil)
+      ((null (tree:rb-tree-l seq)) seq)
+      (t     (first-node (tree:rb-tree-l seq))))))
+
+
+(defun seq::last-node (seq)
+  (let ((seq (value seq)))  
+    (cond ((null seq) nil)
+      ((null (tree:rb-tree-r seq)) seq)
+      (t     (last-node (tree:rb-tree-r seq))))))
+
+
+(defun seq:empty ()
   "create empty seq"
-  (set:empty))
+  nil)
 
 
-(defun is-empty (s)
+(defun seq:empty* ()
+  "create empty seq"
+  (seq:make* (seq:empty)))
+
+
+(defun seq:emptyp (seq)
   "return true if seq contains no elements, otherwise false"
-  (map:is-empty s))
+  (null (value seq)))
   
 
-(defun length (s)
+(defun seq:length (seq)
   "return the number of elements in seq, similar to (cl:length list)"
-  (set:cardinal s))
+  (let ((seq (value seq)))
+    (set:cardinal seq)))
 
 
-(defmethod sequence:length ((s tree:rb-tree))
-  (set:cardinal s))
-
-
-(defun first (s)
+(defun seq:first (seq)
   "return the first element in seq, similar to (cl:first list)"
-  (if (null s)
-    nil
-    (seq-cell-val (set:min s))))
+  (let ((seq (value seq)))
+    (if (null seq)
+      nil
+      (seq-cell-val (set:min seq)))))
 
 
-(defun last (s)
+(defun seq:last (seq)
   "return the last element in seq, similar to (cl:car (cl:last list))"
-  (if (null s)
-    nil
-    (seq-cell-val (set:max s))))
+  (let ((seq (value seq)))
+    (if (null seq)
+      nil
+      (seq-cell-val (set:max seq)))))
 
 
-(defun min-key (s)
+(defun seq::min-key (seq)
   "return the key of the first element of seq"
-  (if (null s)
-    0
-    (seq-cell-key (set:min s))))
+  (let ((seq (value seq)))
+    (if (null seq)
+      0
+      (seq-cell-key (set:min seq)))))
 
 
-(defun max-key (s)
+(defun seq::max-key (seq)
   "return the key of the last element of seq"
-  (if (null s)
-    0
-    (seq-cell-key (set:max s))))
+  (let ((seq (value seq)))
+    (if (null seq)
+      0
+      (seq-cell-key (set:max seq)))))
 
 
-(defun rest (s)
+(defun seq:rest (seq)
   "return a new seq containing all but the first element of s"
-  (if (null s)
-    nil
-    (set:remove-min s)))
+  (let ((seq (value seq)))
+    (if (null seq)
+      nil
+      (set:remove-min seq))))
 
 
-(defun butlast (s)
+(defun seq:butlast (seq)
   "return a new seq containing all but the last element of s"
-  (if (null s)
-    nil
-    (set:remove-max s)))
+  (let ((seq (value seq)))
+    (if (null seq)
+      nil
+      (set:remove-max seq))))
 
 
-(defun push (elem &optional seq)
-  "return a new seq identical to seq but with elem prepended"
+(defun seq:push (elem &optional seq)
+  "return a new seq identical to SEQ but with elem prepended"
   (set:add
-    (make-seq-cell :key (- (min-key seq) 1) :val elem) seq))
+    (make-seq-cell :key (- (min-key seq) 1) :val elem) (value seq)))
 
 
-(defun add (elem &optional seq)
-  "return a new seq identical to seq but with elem appended"
+(defun seq:add (elem &optional seq)
+  "return a new seq identical to SEQ but with elem appended"
   (set:add
-    (make-seq-cell :key (+ (max-key seq) 1) :val elem) seq))
+    (make-seq-cell :key (+ (max-key seq) 1) :val elem) (value seq)))
 
 
-(defun list (s)
-  "return the list-based equivalent to seq s, containing all elements of s in the same order"
-  (mapcar #'seq-cell-val (set:elements s)))
+(defun seq:list (seq)
+  "return the list-based equivalent to SEQ, containing all elements of s in the same order"
+  (mapcar #'seq-cell-val (set:elements (value seq))))
 
 
-(defun dup (s)
-  "return a new seq that is seq:equal to the original seq s"
-  (cond
-    ((null s)  nil)
-    (t         (let* ((min-cell (set:min s))
-                       (new-cell (make-seq-cell
-                                   :key (seq-cell-key min-cell)
-                                   :val (seq-cell-val min-cell))))
-                 (set:add new-cell (dup (set:remove-min s)))))))
+(defun seq:dup (seq)
+  "return a new seq that is seq:equal to the original SEQ"
+  (let ((seq (value seq)))
+    (cond
+      ((null seq)  nil)
+      (t         (let* ((min-cell (set:min seq))
+                         (new-cell (make-seq-cell
+                                     :key (seq-cell-key min-cell)
+                                     :val (seq-cell-val min-cell))))
+                   (set:add new-cell (seq:dup (set:remove-min seq))))))))
 
 
-(defun mapi (f seq)
-  "seq:mapi is functionally identical to map:mapi, with the exception that a new seq is
-   constructed and returned rather than a map.  seq:mapi, however, is not exported as part
+(defun seq::map-indices (f seq)
+  "seq::map-indices is functionally identical to map:keymap, with the exception that a new seq is
+   constructed and returned rather than a map.  seq:map-indices, however, is not exported as part
    of the public interface, as the keys of a seq are managed internally in order to
    manipulate the lexical order of the elements of a seq, and are not user-visible.  That
    said, it is extremely useful when implementing new types of sequence operations
    that may themselves be exported in the public interface as an extension to this library"
-  (cond
-    ((null seq) nil)
-    (t (let (new-seq)
-          (dolist (elem (set:elements seq))
-            (setf new-seq (set:add  (make-seq-cell
-                                      :key (funcall f (seq-cell-key elem))
-                                      :val (seq-cell-val elem)) new-seq)))
-         new-seq))))
+  (let ((seq (value seq)))
+    (cond
+      ((null seq) nil)
+      (t (let (new-seq)
+           (dolist (elem (set:elements seq))
+             (setf new-seq (set:add  (make-seq-cell
+                                       :key (funcall f (seq-cell-key elem))
+                                       :val (seq-cell-val elem)) new-seq)))
+           new-seq)))))
 
 
-(defun concat (s0 &rest args)
+(defun seq::reindex (seq &key (offset 0) (increasing t) &aux (idx offset))
+  (let ((seq (value seq)))
+    (map-indices
+      #'(lambda (key)
+          (declare (ignore key))
+          (prog1 idx
+            (if increasing (incf idx) (decf idx))))
+      seq)))
+
+
+(defun seq::reindex* (seq &key (offset 0) (increasing t))
+  (check-type seq var)
+  (setf (value seq) (reindex seq :offset offset :increasing increasing)))
+  
+
+;; (progn
+;;   (printv (map:keys #1=(push 9 (push :a [1 2 3 4 5]))))
+;;   (printv (map:keys (reindex #1#))))
+;;
+;; (MAP:KEYS          (PUSH 9 (PUSH :A [ 1 2 3 4 5 ])))
+;;      => (-1 0 1 2 3 4 5)
+;; (MAP:KEYS (REINDEX (PUSH 9 (PUSH :A [ 1 2 3 4 5 ]))))
+;;      => (0 1 2 3 4 5 6)
+
+;; (progn
+;;   (printv (map:keys #1=(push 9 (push :a [1 2 3 4 5]))))
+;;   (printv (map:keys (reindex #1# :offset 10 :increasing nil))))
+;;
+;;   (MAP:KEYS (PUSH 9 (PUSH :A [ 1 2 3 4 5 ])))
+;;      => (-1 0 1 2 3 4 5)
+;;   (MAP:KEYS (REINDEX (PUSH 9 (PUSH :A [ 1 2 3 4 5 ])) :OFFSET 10 :INCREASING NIL))
+;;      => (4 5 6 7 8 9 10)
+
+
+(defun seq:reverse (seq)
+  (let ((seq (value seq)))
+    (reindex seq :increasing nil :offset (max-key seq))))
+
+;; (printv (seq:reverse (seq:make (make-gensym-list 10))))
+;;      => [ #:G3836 #:G3835 #:G3834 #:G3833 #:G3832 #:G3831 #:G3830 #:G3829 #:G3828 #:G3827 ]
+
+
+(defun seq:reverse* (seq)
+  (check-type seq var)
+  (reindex* seq :increasing nil :offset (max-key seq)))
+
+;; (printv (reverse* #[1 2 3 4 5 6 7 8]))
+;;      => #[ 8 7 6 5 4 3 2 1 ]
+
+
+(defun seq:concat (s0 &rest args)
   "return a new sequence that consists of all elements of s0 followed by all elements of
    each of the supplied additional sequences, in order, from left to right.  Any number
    of sequences may be concatenated in a single call."
-  (let ((max-key-s0 (max-key s0))
-         (min-key-s1 (if args (min-key (car args)) 0)))        
+  (let* ((s0 (value s0))
+          (s1 (when args (value (car args))))
+          (max-key-s0 (max-key s0))
+          (min-key-s1 (if s1 (min-key s1) 0)))        
     (cond
       ((null args) s0)
       (t (apply #'seq:concat
            (set:union s0
-             (seq::mapi #'(lambda (k) (+ 1
-                                       (if (minusp max-key-s0) 1 max-key-s0)
-                                       (if (minusp min-key-s1) (- min-key-s1) 1)
-                                       k))
-               (car args))) (cl:rest args))))))
+             (seq::map-indices
+               #'(lambda (old-index)
+                   (+ old-index 1
+                     (if (minusp max-key-s0) 1 max-key-s0)
+                     (if (minusp min-key-s1) (abs min-key-s1) 1)))
+               s1))
+           (cl:rest args))))))
 
 
-(defun map (f seq)
+(defun seq:map (f seq)
   "return a new seq that contains the result of applying f to each element of seq,
    in the same lexical order.  The underlying structure of the collection rb-tree
    is unaffected.  Operates in a manner analogous to cl:mapcar."
-   (cond ((null seq) nil)
-     (t (let (new-seq)
-          (dolist (elem (set:elements seq))
-            (setf new-seq (set:add (make-seq-cell
+  (let ((seq (value seq)))
+    (cond ((null seq) nil)
+      (t (let (new-seq)
+           (dolist (elem (set:elements seq))
+             (setf new-seq (set:add (make-seq-cell
                                       :key (seq-cell-key elem)
-                                     :val (funcall f (seq-cell-val elem)))
-                            new-seq)))
-          new-seq))))
+                                      :val (funcall f (seq-cell-val elem)))
+                             new-seq)))
+           new-seq)))))
 
 
-(defun make (&optional (from (seq:empty)))
+;; (seq:equal (seq:map #'string          [:a :b :c :d]) ["A" "B" "C" "D"])
+;; (seq:equal (seq:map #'princ-to-string [5 4 3 2 1])   ["5" "4" "3" "2" "1"])
+  
+
+(defun seq:make (&optional (from (seq:empty)))
   "end-user api for construction of a new seq optionally initialized to contain
    elements derived from various types of source data, with preservation of the
    original order in such cases where the concept of lexical ordering applies."
   (etypecase from
     (null     (seq:empty))
-    (dstm:var (seq:make (dstm:atomic (dstm:read from))))
-    (cl:list  (let (seq)
-                (dolist (elem from)
-                  (setq seq (seq:add elem seq)))
-                seq))
+    (var      (seq:make (value from)))
+    (cl:list  (let (seq) (dolist (elem from) (setq seq (seq:add elem seq))) seq))
     (seq:type (seq:dup from))
     (map:type (error "at this point seqs cannot be created from ordinary maps"))
     (set:type (seq:make (set:elements from)))
-    (sequence (seq:make (coerce from 'cl:list)))
-    (atom     (seq:add from))))
+    (string   (seq:add from (seq:empty)))
+    (sequence (seq:make (cl:coerce from 'cl:list)))
+    (atom     (seq:add from (seq:empty)))))
 
 
-(defun make* (&optional (from (seq:empty)))
-  (dstm:create-var (seq:make from) 'seq:seq*))
+(defun seq:make* (&optional (from (seq:empty)))
+  ;; selects stm implementation based on class of seq*
+  (if (find (find-class 'mutable-seq/cstm)
+        (c2mop:class-direct-superclasses (find-class 'dclx:seq*)))
+    (cstm:create-var (seq:make from) 'seq*)
+    (dstm:create-var (seq:make from) 'seq*)))
 
-(defun elt (index seq)
+
+(defun seq:elt (seq index)
   "return the element of seq at position index, which should be a positive integer
-   as would apply in a manner analogous to using cl:elt on a built-in sequence type"
-  (cl:elt (seq:list seq) index))
+   as would apply in a manner analogous to using cl:elt on a built-in sequence type."
+  (when (minusp index) (error "dereference index: ~d before start of seq: ~d" index 0))
+  (let* ((seq (value seq))
+          (enum (tree:cons-enum seq nil))
+          (node (progn
+                  (when (plusp index)
+                    (dotimes (i index)
+                      (when (every #'null (cl:rest enum))
+                        (error (make-condition 'sb-kernel:index-too-large-error
+                                 :expected-type `(integer 0 ,(- (seq:length seq) 1))
+                                 :datum index)))
+                      (setq enum (tree:cons-enum (cl:second enum) (cl:third enum)))))
+                  (cl:first enum))))
+    (if node
+      (values (seq-cell-val node) t)
+      (values nil t))))
 
 
-(defun reduce (fn seq &key key from-end (start 0) end initial-value)
+(defun seq::elt-impl2 (seq index)
+  (let* ((seq (value seq))
+          (list (seq:list seq)))
+    (cl:elt list index)))
+
+
+;; (elt  [1 2 3 4 5] -1) (elt-impl2  #[1 2 3 4 5] -1)
+;; (elt  [1 2 3 4 5]  0) (elt-impl2  #[1 2 3 4 5]  0)
+;; (elt  [1 2 3 4 5]  1) (elt-impl2  #[1 2 3 4 5]  1)
+;; (elt  [1 2 3 4 5]  2) (elt-impl2  #[1 2 3 4 5]  2)
+;; (elt  [1 2 3 4 5]  3) (elt-impl2  #[1 2 3 4 5]  3)
+;; (elt  [1 2 3 4 5]  4) (elt-impl2  #[1 2 3 4 5]  4)
+;; (elt  [1 2 3 4 5]  5) (elt-impl2  #[1 2 3 4 5]  5)
+;; (elt  [1 2 3 4 5]  6) (elt-impl2  #[1 2 3 4 5]  6)
+
+
+(defun seq:reduce (fn seq &rest args &key key from-end (start 0) end initial-value)
   "combines the elements of seq according to function and returns the result.
    for example, a sequence of numbers may be reduced using function #'+ which
    would return the result of adding them all together.  reduction using #'cl:max
@@ -253,19 +351,22 @@
    of seq are #[a b c d] calling reduce is equivalent to (fn (fn (fn a b) c) d).
    All options of #'cl:reduce are supported as it is what is used in the underlying
    implementation of #'seq:reduce"
-  (cl:reduce fn (seq:list seq)
-    :key key
-    :from-end from-end
-    :start start
-    :end end
-    :initial-value initial-value))
+  (declare (ignorable key from-end start end initial-value))
+  (apply #'cl:reduce (cl:append (cl:list fn (seq:list (value seq))) args)))
 
 
+;; (seq:reduce #'cl:+   [1 2 3 4 5])
+;; (seq:reduce #'cl:max [1 2 3 4 5])
+;; (seq:reduce #'cl:min [1 2 3 4 5])
+;; (seq:reduce #'cl:*   [1 2 3 4 5])
 
-(defun compare (seq1 seq2 &optional (cmp #'ord:compare))
+
+(defun seq:compare (seq1 seq2 &optional (cmp #'ord:compare))
   "ordinal comparison of two seqs"
-  (let ((e1 (tree::cons-enum seq1 nil))
-         (e2 (tree::cons-enum seq2 nil)))
+  (let*  ((seq1 (value seq1))
+          (seq2 (value seq2))
+          (e1   (tree:cons-enum seq1 nil))
+          (e2   (tree:cons-enum seq2 nil)))
     (tagbody again
       (return-from compare
         (cond
@@ -280,9 +381,22 @@
                      (t               (progn
                                         (setf e1 (tree::cons-enum r1 ee1)
                                               e2 (tree::cons-enum r2 ee2))
-                                        (go again)) ))) ))))) )))
+                                        (go again)))))))))))))
 
 
-(defun equal (seq1 seq2 &optional (cmp #'ord:compare))
+(defun seq:equal (seq1 seq2 &optional (cmp #'ord:compare))
   "compare two seqs for element-wise equality using cmp"
-  (zerop (compare seq1 seq2 cmp)))
+  (zerop (seq:compare (value seq1) (value seq2) cmp)))
+
+
+;; (seq:equal [1 2 3 4 5]   [1 2 3 4 5])
+;; (seq:equal [1 2 3 4 5]   [1 2 3 4])
+;; (seq:equal [1 :b 3 :c 5] [1 :b 3 :c 5])
+
+;; (not (eq #1=[1 2 3 4 5] (seq:dup #1#)))
+;; (eq #1=[1 2 3 4 5] #1#)
+
+
+#+()
+(defmethod sequence:length ((s tree:rb-tree))
+  (set:cardinal s))
