f4674b47f6ba4652c0bad06f3a54269471859f37
dan lentz <danlentz@gmail.com>
2012-04-18 00:06:50 -0400

    redesigned package structure; migration of package defs for utility packages: ord, cstm, lock, io, mmap, pointer, etc

--- package.lisp
+++ package.lisp
@@ -3,19 +3,36 @@
 
 (in-package :cl-user)
 
-
-(defpackage :dstm-collections
-  (:nicknames :dclx)
-  (:use :common-lisp :named-readtables)
+(defpackage :dstm-collections (:nicknames :cxn :dclx :collex)
+  (:documentation "")
+  (:use :common-lisp :contextl :named-readtables)
   (:export
     :?
     :?+
     :printv
-    :var
-    :value
     :with-gensyms
     :make-gensym-list
+    :make-keyword
+    :symbolicate
+    :keywordicate
     :once-only
+    :nlet
+    :symbolic
+    :scond
+    :scase
+    :swhen
+    :anaphoric
+    :acase
+    :atypecase
+    :awhen
+    :aprog1
+    :conc1f
+    :reuse-cons
+    :ensure-function
+    :compose
+    :multiple-value-compose
+    :ensure-list
+    :ppmx
     :ensure-kernel
     :standard-syntax
     :*default-syntax*
@@ -27,279 +44,233 @@
     :*parallel-execution-enabled*
     :*default-syntax-startup-enabled*
     :*print-collections-readably*
-   ))
+    :timing-info
+    :real-time
+    :user-time
+    :system-time
+    :gc-time
+    :page-faults
+    :bytes-consed
+    :collect-timing
+    :pprint-bytes
+    :pprint-milliseconds
+    :with-profiling-enabled
+    :execution-time
+    :specification
+    :pprint-to-string
+    :report-and-ignore-errors
+    :s
+    :ps
+    :pcond
+    :plet
+    :plet*
+    :class-proto
+    :using))
 
 
-(defpackage :quad
-  (:use :common-lisp :lparallel :named-readtables)
-  (:import-from :dclx :? :?+ :printv)
-  (:export
-    :tuple
-    :abstract-quad
-    :qar
-    :qbr
-    :qcr
-    :qdr
-    :a
-    :b
-    :c
-    :d))
-
-
-(defpackage :mmap
-  (:use :common-lisp)
-  (:export
-    :string-to-octets
-    :octets-to-string
-    :make-buffer
-    :mmap-stream
-    :mmap-stream-pathname
-    :mmap-stream-length
-    :open-mmap-stream
-    :close-mmap-stream))
-    
-
-(defpackage :cstm
+(defpackage :tree
+  (:documentation "")
   (:shadowing-import-from :closer-mop :standard-generic-function :defgeneric :defmethod)
-  (:use :common-lisp :contextl :closer-mop :lparallel :named-readtables)
-  (:import-from :dclx :? :?+ :printv :var :value)
+  (:use :common-lisp :collex :contextl :closer-mop)
   (:export
-    :*tries*
-    :*timeout*
-    :*current-transaction*
-    :deferred-update-mode
-    :direct-update-mode
-    :transaction
-    :define-transactional-class
-    :transactional-class
-    :transactional-variable
-    :value
-    :var
-    :create-var
-    :call-atomic
-    :atomic
-    :roll-back
-    :commit-transaction
-    :retry-transaction
-    :most-recent-transaction
-    :transaction-status))
-
-
-(defpackage :dstm
-  (:use :common-lisp :lparallel :named-readtables)
-  (:import-from :dclx :? :?+ :printv :var :value)
-  (:shadow :read :write)
-  (:export
-    :var
-    :dstm-var
-    :*transaction*
-    :transaction
-    :create-var
-    :read
-    :write    
-    :atomic
-    :orelse
-    :rollback
-    :with-update
-    :updatef
-    :safe-value
-    :value
-    :check
-    :reset))
-
-
-(defpackage :ord
-  (:use :common-lisp :lparallel :named-readtables)
-  (:import-from :dclx :? :?+ :printv)
-  (:export
-    :compare|>|
-    :compare|<|
-    :compare|<=|
-    :compare|=|
-    :compare|>=|
-    :compare
-    :make-ci-char
-    :make-ci-string
-    :slots-to-compare
-    :writing-readably
-    :of-type
-    :proper-list
-    :proper-list-p
-    :association-list
-    :association-list-p
-    ))
-
-
-(defpackage :rbtree
-  (:nicknames :rb :tree) ;; tree is deprecated
-  (:use :common-lisp :quad :lparallel :named-readtables)
-  (:import-from :dclx :? :?+ :printv :value :var)
-  (:shadow :merge :typep :type  :min  :max)
-  (:export
-    :rb-tree
-    :make-rb-tree
-    :make-cursor
-    :with-cursor
-    :rb-tree-p
-    :rb-tree-l
-    :rb-tree-v
-    :rb-tree-r
-    :rb-tree-h
-    :create
-    :height
-    :weight
-    :add
-    :min
-    :max
-    :remove-min
-    :remove-max    
-    :bal
-    :join
-    :merge
-    :concat
-    :cons-enum
+    :balanced
+    :height-balanced
+    :weight-balanced
+    :merge-direction
+    :leaf
+    :empty
+    :empty?
+    :node
+    :kv
     :lr
-    :lvr
-    :lvrh
-    :typep
-    :type
-    ))
+    :kvlr
+    :kvlrx
+    :kvlrs
+    :kvlrh
+    :node/k
+    :node/v
+    :node/l
+    :node/r
+    :node/x
+    :node/s
+    :node/h
+    :node/kv
+    :node/lr
+    :node/kvlr
+    :node/kvlrx
+    :node/kvlrs
+    :node/kvlrh
+    :node/constituents
+    :node/values
+    :node/call
+    :node/size
+    :node/weight
+    :node/height
+    :node/create
+    :node/join
+    :node/singleton
+    :node/least
+    :node/greatest
+    :node/remove-least
+    :node/remove-greatest
+    :node/concat2
+    :node/inorder-fold
+    :node/iter
+    :node/at-index
+    :node/find
+    :node/add
+    :node/remove
+    :node/concat3
+    :node/concat
+    :node/split-lesser
+    :node/split-greater
+    :node/split
+    :node/union
+    :node/union-merge
+    :node/intersection
+    :node/difference
+    :node/subset?
+    :node/from
+    :node/member?
+    :node/cons-enum
+    :node/contains?
+    :node/rank
+    :node/for-all
+    :+delta+
+    :+gamma+
+    :node/empty?
+    :layer
+    :allocation))
 
 
 (defpackage :set
-  (:use :common-lisp :lparallel :named-readtables)
-  (:import-from :dclx :? :?+ :printv :value :var :with-gensyms :make-gensym-list :once-only)
-  (:import-from :tree :create :bal :join :concat :cons-enum 
-    :lr :lvr :lvrh :make-rb-tree :rb-tree-p :rb-tree-l :rb-tree-v :rb-tree-r :rb-tree-h
-    :height :add :remove-min :remove-max :make-cursor :with-cursor)
-  (:shadowing-import-from :tree :merge  :max :min)
-;;  (:shadowing-import-from :dclx :coerce)
+  (:shadowing-import-from :closer-mop :standard-generic-function :defgeneric :defmethod)
+  (:use :common-lisp :closer-mop :contextl :collex :tree :named-readtables)
   (:shadow :equal :remove :union :typep :type :set :do)
   (:export
-    :syntax
-    :set           :set*
-    :empty         :empty*
-    :make          :make*    
-    :add           :add*
-    :singleton     :singleton*
-    :remove        :remove*
-    :remove-min    :remove-min*
-    :remove-max    :remove-max*
-    :union         :union*
-    :inter         :inter*
-    :filter        :filter*    
-    :height
-    :emptyp
-    :make-cursor
-    :with-cursor
-    :dup
-    :mem
-    :min
-    :max
-    :diff
-    :compare
-    :equal
-    :subset
-    :iter
-    :do
-    :fold
-    :for-all
-    :exists
-    :split
-    :partition
-    :cardinal
-    :elements
+    :set
+    :set*
     :typep
     :type
-    ))
+    :add
+    :least
+    :greatest
+    :remove-least
+    :remove-greatest
+    :isolated-transaction
+    :direct-update-tranaction
+    :deferred-update-tranaction
+    :direct-update-tranaction/height-balanced 
+    :direct-update-tranaction/weight-balanced
+    :deferred-update-tranaction/weight-balanced    
+    :deferred-update-tranaction/height-balanced
+    :isolated-tranaction/height-balanced
+    :isolated-tranaction/weight-balanced
+    :istm/rb
+    :istm/wb
+    :cstm/rb    
+    :cstm/wb
+    :dstm/wb
+    :dstm/rb))   
 
 
 (defpackage :map
-  (:use :common-lisp :lparallel :named-readtables)
-  (:shadow :find :equal :map :remove :typep :type :values :do)
-  (:import-from :set :dup)
-  (:import-from :dclx :? :var :value)
-;;  (:shadowing-import-from :dclx :coerce)
- (:import-from :tree :lr :lvr :lvrh :cons-enum :make-cursor :with-cursor)
+  (:shadowing-import-from :closer-mop :standard-generic-function :defgeneric :defmethod)
+  (:use :common-lisp :closer-mop :contextl :collections :tree :named-readtables)
   (:export
-    :map          :map*
-    :empty        :empty*
-    :make         :make*
-    :add          :add*
-    :ensure-find  :ensure-find*      
-    :remove       :remove*
-    :emptyp
-    :find
-    :mem
-    :iter
-    :keys
-    :values
-    :map
-    :mapi
-    :keymap
-    :do
-    :fold
-    :compare
-    :equal
-    :typep
-    :type
-    :dup
-    :make-cursor
-    :with-cursor
-    :syntax
-;;    :coerce
-    ))
+    :map          :map*))
+
+;;   (:use :common-lisp :lparallel :named-readtables)
+;;   (:shadow :find :equal :map :remove :typep :type :values :do)
+;;   (:import-from :set :dup)
+;;   (:import-from :dclx :? :var :value)
+;; ;;  (:shadowing-import-from :dclx :coerce)
+;;  (:import-from :tree :lr :lvr :lvrh :cons-enum :make-cursor :with-cursor)
+;;   (:export
+;;     :map          :map*
+;;     :empty        :empty*
+;;     :make         :make*
+;;     :add          :add*
+;;     :ensure-find  :ensure-find*      
+;;     :remove       :remove*
+;;     :emptyp
+;;     :find
+;;     :mem
+;;     :iter
+;;     :keys
+;;     :values
+;;     :map
+;;     :mapi
+;;     :keymap
+;;     :do
+;;     :fold
+;;     :compare
+;;     :equal
+;;     :typep
+;;     :type
+;;     :dup
+;;     :make-cursor
+;;     :with-cursor
+;;     :syntax
+;; ;;    :coerce
+;;     ))
 
 
 (defpackage :seq
+  (:shadowing-import-from :closer-mop :standard-generic-function :defgeneric :defmethod)
+  (:use :common-lisp :closer-mop :contextl :collections :tree :named-readtables)
   (:shadow  :push  :pop    :first    :second    :third    :elt :butlast
     :last  :rest  :length    :map    :equal    :dup      :typep :list :vector
     :type :reduce :do :reverse :subseq)
-  (:import-from :tree :make-cursor :with-cursor)
-  (:import-from :dclx :? :?+ :printv :var :value :with-gensyms :once-only :make-gensym-list)
-  (:use :common-lisp :lparallel :named-readtables)
   (:export
-    :reverse :reverse*
-    :seq     :seq*
-    :empty   :empty*
-    :make    :make*
-    :add     :add*
-    :push    :push*
-    :emptyp
-    :first
-    :last
-    :rest
-    :butlast
-    :butfirst
-    :list
-    :vector
-    :length
-    :subseq
-    :dup
-    :typep
-    :type    
-    :concat
-    :do
-    :map
-    :elt
-    :compare
-    :equal
-    :reduce
-    :syntax
-    :make-cursor
-    :with-cursor
-    ))
+    :seq     :seq*))
+
+  
+  ;; (:import-from :tree :make-cursor :with-cursor)
+  ;; (:import-from :dclx :? :?+ :printv :var :value :with-gensyms :once-only :make-gensym-list)
+  ;; (:use :common-lisp :lparallel :named-readtables)
+  ;; (:export
+  ;;   :reverse :reverse*
+  ;;   :seq     :seq*
+  ;;   :empty   :empty*
+  ;;   :make    :make*
+  ;;   :add     :add*
+  ;;   :push    :push*
+  ;;   :emptyp
+  ;;   :first
+  ;;   :last
+  ;;   :rest
+  ;;   :butlast
+  ;;   :butfirst
+  ;;   :list
+  ;;   :vector
+  ;;   :length
+  ;;   :subseq
+  ;;   :dup
+  ;;   :typep
+  ;;   :type    
+  ;;   :concat
+  ;;   :do
+  ;;   :map
+  ;;   :elt
+  ;;   :compare
+  ;;   :equal
+  ;;   :reduce
+  ;;   :syntax
+  ;;   :make-cursor
+  ;;   :with-cursor
+  ;;   ))
 
 
-(shadowing-import '(;set:set
-                     set:set*
-                     ;map:map
-                     map:map*
-                     ;seq:seq 
-                     seq:seq*) :dclx)
-(export '(;dclx::set
-           dclx::set*
-           ;dclx::map
-           dclx::map*
-           ;dclx::seq
-           dclx::seq*) :dclx)
+;; (shadowing-import '(;set:set
+;;                      set:set*
+;;                      ;map:map
+;;                      map:map*
+;;                      ;seq:seq 
+;;                      seq:seq*) :dclx)
+;; (export '(;dclx::set
+;;            dclx::set*
+;;            ;dclx::map
+;;            dclx::map*
+;;            ;dclx::seq
+;;            dclx::seq*) :dclx)
