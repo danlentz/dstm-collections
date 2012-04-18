2e6ca5e0b6d7337c62a56445f8f1b48367d5309c
dan lentz <danlentz@gmail.com>
2012-04-18 05:24:36 -0400

    disable lparallel integration

--- dstm-collections.lisp
+++ dstm-collections.lisp
@@ -1,22 +1,23 @@
 ;;;;; -*- mode: common-lisp;   common-lisp-style: modern;    coding: utf-8; -*-
 ;;;;;
 
-(in-package :dclx)
+(in-package :collex)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defvar *default-syntax*                 'dclx:standard-syntax))
+  (defvar *default-syntax*                 'standard-syntax))
 
-
+(defvar *prior-readtable*                nil)
 (defvar *default-syntax-startup-enabled* t)                                                         
 (defvar *print-collections-readably*     t)
 (defvar *set-reader-macro-char*          #\{)
 (defvar *seq-reader-macro-char*          #\[)
-(defvar *value-reader-macro-char*        #\$)
-(defvar *prior-readtable*                nil)
-(defvar *parallel-execution-enabled*     t)
-(defvar *default-kernel-parallelism*     16)
-(defvar *default-kernel-startup-enabled* t)
-(defvar *kernel*                         nil)
+#+() (defvar *value-reader-macro-char*        #\$)
+
+
+#+() (defvar *parallel-execution-enabled*     t)
+#+() (defvar *default-kernel-parallelism*     16)
+#+() (defvar *default-kernel-startup-enabled* t)
+#+() (defvar *kernel*                         nil)
 
 
 (defun default-syntax ()
@@ -26,17 +27,19 @@
 (defun enable-syntax ()
   (unless (eq *readtable* (default-syntax))
     (setf *prior-readtable* *readtable*)
-    (funcall #'in-readtable (default-syntax))))
+    (setf *readtable* (default-syntax))))
 
 
 (defun disable-syntax ()
-  (in-readtable *prior-readtable*))
+  (unless (null *prior-readtable*)
+    (setf *readtable* *prior-readtable*)
+    (setf *prior-readtable* nil)))
 
 #+()
 (defun kernel ()
   *kernel*)
 
-
+#+()
 (defun ensure-kernel ()
   (unless *kernel*
     (setf *kernel*
@@ -49,17 +52,3 @@
     (warn "lparallel:*kernel* already running. dclx:*kernel* started but is not global default"))
   :kernel-started)
     
-
-(defclass   var ()())
-
-(defgeneric value (thing)
-  (:documentation "Atomically read the value of a transactional variable"))
-
-(defgeneric (setf value) (new-value var))
-
-(defmethod  value ((thing t))
-  thing)
-
-(defmethod (setf value) (new-value (var t))
-  (error "Attempt to write the VALUE of the NON-TRANSACTIONAL place ~S" var))
-
